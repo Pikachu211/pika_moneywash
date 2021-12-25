0ab2d06bfaadaffdf1862742f36fe599bd0a8725
Pikachu211 <96638847+Pikachu211@users.noreply.github.com>
2021-12-25 03:13:00 +0100

    Update config.lua

--- pika_moneywash/config.lua
+++ pika_moneywash/config.lua
@@ -3,3 +3,7 @@ Config = {}
 Config.Location = vector3(1126.0, -1242.0, 22.0)
 Config.MaxDistance = 4
 Config.ClickDistance = 2
+
+Config.Map = {
+  {name="Money Wash",  		color=6, scale=1, id=500, x = 1126.0, y = -1241.8, z = 21.5}
+}
